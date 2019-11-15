c3de35f9e413e93298f57a5a7b9a9e70a6463bd8
zhangjunjie <sh-zhangjj7@chinaunicom.cn>
2019-11-15 16:58:38 +0800

    test

--- src/components/listSingal/index.vue
+++ src/components/listSingal/index.vue
@@ -2,6 +2,7 @@
 	<div class="listSingal">
 		<img :src="'../../pic/' + pic + '.jpg'" v-if="pic != '' && pic != undefined && pic != null" />
 		<strong>{{index}}.{{problem}}</strong>
+		<!-- <button @click="setRemember()">加入回忆题库</button> -->
 		<dl>
 			<dd v-for="(item,index) in answer">
 				<input type="radio" :name="problem" :id="problem + index" v-if="$store.state.done == false" :attr-answer="item" @click="checkAnswer($event)" />
@@ -33,6 +34,26 @@ export default{
 	methods:{
 		checkAnswer(e){
 			this.mykey = e.currentTarget.getAttribute('attr-answer')
+		},
+		setRemember(){
+			let tempRemember = []
+			let tempCounter = 0
+			if(!localStorage.getItem('remember')){
+				localStorage.setItem('remember',JSON.stringify(tempRemember))
+			}else{
+				tempRemember = JSON.parse(localStorage.getItem('remember'))
+			}
+			tempRemember.map((item,index) => {
+				if(item.problem == this.problem){
+					tempCounter ++
+				}
+			})
+			if(tempCounter == 0){
+				tempRemember.push({problem:this.problem,answer:this.answer,key:this.trueanswer,explain:this.explain,pic:this.pic})
+			}else{
+				alert('此题已经出现在回忆题库中！')
+			}
+			localStorage.setItem('remember',JSON.stringify(tempRemember))
 		}
 	}
 }
